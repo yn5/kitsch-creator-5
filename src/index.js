5fedb05a79dce1bb40f215088ef7e123b5ceedde
yn5 <ynsnetmail@gmail.com>
2018-07-24 03:40:39 +0200

    Add css reset

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import './styles/reset';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
