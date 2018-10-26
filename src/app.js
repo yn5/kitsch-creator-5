4f2a0d278986f786f6d98e657248e6fcbc48da76
yn5 <ynsnetmail@gmail.com>
2018-10-26 22:16:27 +0200

    Replace styled-components injectGlobal with createGlobalStyles

--- src/app.js
+++ src/app.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import GlobalStyle from './styles/reset';
 import BobrossSketchView from './components/views/bobross-sketch-view';
 import HomeView from './components/views/home-view';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route
-          component={HomeView}
-          path="/"
-          exact
-        />
-        <Route
-          component={BobrossSketchView}
-          path="/sketches/bobross"
-          exact
-        />
-      </Switch>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route
+            component={HomeView}
+            path="/"
+            exact
+          />
+          <Route
+            component={BobrossSketchView}
+            path="/sketches/bobross"
+            exact
+          />
+        </Switch>
+      </BrowserRouter>
+      <GlobalStyle />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import './styles/reset';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
