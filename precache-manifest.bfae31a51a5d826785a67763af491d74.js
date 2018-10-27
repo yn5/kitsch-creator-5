389be8a46cbc67ee596a3f4aa4ef9997d96558a8
yn5 <ynsnetmail@gmail.com>
2018-10-27 15:49:45 +0200

    Updates

---
+++ precache-manifest.bfae31a51a5d826785a67763af491d74.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "df127fd8b2f9108652f6",
+    "url": "/kitsch-creator-5/static/js/runtime~main.df127fd8.js"
+  },
+  {
+    "revision": "f88327db67d0582277d1",
+    "url": "/kitsch-creator-5/static/js/main.f88327db.chunk.js"
+  },
+  {
+    "revision": "076c04f7b2dc7b819e40",
+    "url": "/kitsch-creator-5/static/js/1.076c04f7.chunk.js"
+  },
+  {
+    "revision": "fb5561acc2bd830b289172bec5c72c43",
+    "url": "/kitsch-creator-5/index.html"
+  }
+];
